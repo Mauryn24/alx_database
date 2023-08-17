ef25cbbe3057795a16d37fdcb049bf6eb21c4845
Mauryn24 <johndoe@example.com>
2023-08-17 18:00:14 +0300

    data manipulation and definition

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,9 @@
 -- creates table second_table in the database hbtn_0c_0
-CREATE table IF NOT EXISTS second_table ('id' INT,
-'name' VARCHAR(256),
-'score' INT);
+CREATE TABLE IF NOT EXISTS second_table (
+    'id' INT,
+    'name' VARCHAR(256),
+    'score' INT
+);
 INSERT INTO 'second_table' ('id', 'name', 'score')
 VALUES
         (1, 'John', 10),
