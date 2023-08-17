5a3299dc8a21b6e37c3962e063df38b022bcac2f
Mauryn24 <johndoe@example.com>
2023-08-17 21:51:57 +0300

    MORE QUERIES

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- create a user and password with select priviliges
 CREATE database IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE on *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILIGES;
