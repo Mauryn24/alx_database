13994ceaf794f3c280eebfe90e49954321b4b64a
Mauryn24 <johndoe@example.com>
2023-10-22 16:20:10 +0300

    creates the database hbtn_0d_2 and the user user_0d_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
--- create a user and password with select priviliges
-CREATE database IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT USAGE on *.* TO 'user_0d_2'@'localhost';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILIGES;
+-- create the database hbtn_0d_2 and the user user_0d_2 with only SELECT privilege on the hbtn_0d_2 database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
